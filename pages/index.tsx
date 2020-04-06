6dc6451f339ced3f33145e3808ef5926b8be5d44
André Batista <hi@andrebatista.dev>
2020-04-06 12:01:25 +0100

    refactor the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,65 @@
 import React from 'react';
+import { GetStaticProps } from 'next';
+import { styled } from '@material-ui/styles';
 
-const HomePage = ({}: HomePageProps) => {
+// Components
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import PageLayout from 'components/PageLayout';
+
+const HomePage = ({ pageMeta }: HomePageProps) => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <PageLayout pageMeta={pageMeta}>
+      <Wrapper>
+        <Typography variant="h4" component="h1">
+          HELLO WORLD
+        </Typography>
+        <Typography variant="subtitle1" component="h2">
+          Next.js Boilerplate by{' '}
+          <Link
+            href="https://github.com/andrewizbatista"
+            title="@andrewizbatista on GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @andrewizbatista
+          </Link>
+        </Typography>
+        <Typography variant="caption" component="p">
+          Good luck on your new project :)
+        </Typography>
+      </Wrapper>
+    </PageLayout>
   );
 };
 
-HomePage.getInitialProps = async (): Promise<HomePageProps> => {
-  return {};
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      pageMeta: {
+        metaTitle: 'Next.js Boilderplate',
+        metaDescription: 'Next.js Boilderplate by @andrewizbatista',
+      },
+    },
+  };
 };
 
-export interface HomePageProps {}
+export interface HomePageProps {
+  pageMeta: PageMeta;
+}
 
 export default HomePage;
+
+const Wrapper = styled('div')({
+  textAlign: 'center',
+  '& h1': {
+    fontWeight: 'bold',
+    paddingTop: '10rem',
+  },
+  '& h2': {
+    paddingTop: '0.25rem',
+  },
+  '& p': {
+    paddingTop: '2rem',
+  },
+});
