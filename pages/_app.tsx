22e141359ae42d798287aae7a0f99039d20e949d
André Batista <hi@andrebatista.dev>
2020-04-14 15:51:40 +0100

    add created by console log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,15 @@ class MyApp extends App {
     if (jssStyles) {
       jssStyles.parentNode!.removeChild(jssStyles);
     }
+
+    const consoleStyles = [
+      'font-size: 1rem',
+      'font-weight: bold',
+      `color: ${theme.palette.primary.main}`,
+    ].join(';');
+
+    console.log('%cNext.js Boilerplate by @andrewizbatista', consoleStyles); // eslint-disable-line no-console
+    console.log('https://github.com/andrewizbatista/nextjs-boilerplate'); // eslint-disable-line no-console
   }
 
   public render() {
